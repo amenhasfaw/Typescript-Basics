54756cc5f3e433e1f8e12ac90cc16269552b086b
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-03-17 11:07:21 +0530

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // Basic static typing in Typescript
 
-let x: number = 10;
+let id: number = 10;
 
-// x = 'Hello World'  
+// z = 'Hello World'  
 
 // Shows an error because of wrong type
 // index.ts:5:1 - error TS2322: Type 'string' is not assignable to type 'number'.
