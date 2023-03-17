d385f6b18d313e015d50fd2a4dead75798f6fbb8
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-03-17 11:14:27 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,8 @@ let id: number = 10;
 
 // Shows an error because of wrong type
 // index.ts:5:1 - error TS2322: Type 'string' is not assignable to type 'number'.
+
+
+//tsconfig.json
+// Changed outdir to ./dist
+// Changed root directory to ./src
